e36e4037f4f82a1b3429edfda055cd3877c069f0
GustavoPeresDev <gustavoperes2004@gmail.com>
2025-10-01 15:09:57 -0300

    abrir interface ninja repository

---
+++ CadastroDeNinjas/src/main/java/dev/javinhasedex/CadastroDeNinjas/Ninjas/Controller/Service/NinjaRepository.java
@@ -0,0 +1,7 @@
+package dev.javinhasedex.CadastroDeNinjas.Ninjas.Controller.Service;
+
+import dev.javinhasedex.CadastroDeNinjas.Missoes.MissoesModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
+}
