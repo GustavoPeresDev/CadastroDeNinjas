46baa31af5fa9067835fe7ee5943448a22d00e4f
GustavoPeresDev <gustavoperes2004@gmail.com>
2025-10-01 10:24:24 -0300

    abri interface de missoes repository

---
+++ CadastroDeNinjas/src/main/java/dev/javinhasedex/CadastroDeNinjas/Missoes/MissoesRepositor.java
@@ -0,0 +1,6 @@
+package dev.javinhasedex.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepositor extends JpaRepository<MissoesModel,Long> {
+}
