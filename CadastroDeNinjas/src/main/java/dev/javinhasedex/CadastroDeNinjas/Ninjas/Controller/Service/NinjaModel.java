a819391fd57b0426e75855821f5cc21ad515a6bf
GustavoPeresDev <gustavoperes2004@gmail.com>
2025-10-01 09:08:24 -0300

    feat: Coloca o Lomvok na classe para abs

--- CadastroDeNinjas/src/main/java/dev/javinhasedex/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/javinhasedex/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -2,10 +2,15 @@ package dev.javinhasedex.CadastroDeNinjas.Ninjas.Controller.Service;
 
 import dev.javinhasedex.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
 @Table(name = "tb_cadastro")
-
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -22,37 +27,5 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
 
